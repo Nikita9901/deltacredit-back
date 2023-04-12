e43555fc830c56374234987c858e3ea536155316
Nikita9901 <nikitichsn2@gmail.com>
2023-04-12 21:23:43 +0300

    feat: create base back

--- services/token-service.js
+++ services/token-service.js
@@ -7,11 +7,11 @@ class TokenService {
       payload,
       process.env.JWT_ACCESS_SECRET_KEY,
       {
-        expiresIn: '10s',
+        expiresIn: '60s',
       }
     );
     const refreshToken = jwt.sign(payload, process.env.JWT_REFRESH_SECRET_KEY, {
-      expiresIn: '20s',
+      expiresIn: '30d',
     });
     return {
       accessToken,
